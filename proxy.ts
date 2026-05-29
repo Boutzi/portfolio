e3850b44666506afadfc4866f1ba9bb641f21ac0
Boutzi <joe@arkaans.com>
2026-05-29 14:31:10 +0200

    fix lang

--- proxy.ts
+++ proxy.ts
@@ -3,13 +3,9 @@ import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
 
-// export const config = {
-//   matcher: [
-//     "/",
-//     "/(fr|en|it|kr|jp)/:path*",
-//     "/((?!api|_next|_vercel|.*\\..*).*)",
-//   ],
-// };
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  matcher: ["/", "/(fr|en|it|kr|jp)/:path*", "/((?!api|_next|_vercel|.*\\..*).*)"],
 };
+// export const config = {
+//   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+// };
