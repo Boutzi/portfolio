86351be30c5b9c68184492c5f1f6e70a9f99e9b7
Boutzi <joe@arkaans.com>
2026-05-30 17:11:22 +0200

    try testing

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,13 @@
-import type { Config } from "jest";
+/** @type {import('jest').Config} */
 
-const config: Config = {
+const config = {
   preset: "ts-jest",
   silent: true,
   testEnvironment: "node",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
+  transformIgnorePatterns: ["node_modules/(?!(next-intl|next|@next)/)"],
 };
 
 export default config;
